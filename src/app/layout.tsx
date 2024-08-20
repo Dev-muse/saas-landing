6cb29bab0d2d5fa92f48c7b2f5da973edd02a379
Rahman <museabdirahman@gmail.com>
2024-08-20 22:40:24 +0100

    updated footer and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import Logo from "@/assets/logosaas.png";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "TrackBit: Your Ultimate Tracking Solution",
+  description:
+    "Effortlessly track assets, people, and events in real-time. TrackBit offers advanced features, customizable alerts, and intuitive analytics for optimal efficiency and security.",
+  keywords:
+    "tracking app, GPS tracking, asset management, fleet management, real-time monitoring, geofencing, security",
 };
 
 export default function RootLayout({
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -28,9 +28,7 @@ export const Footer = () => {
           <SocialPin />
           <SocialYoutube />
         </div>
-        <p className="mt-6">
-          &copy; 2024 Your Company, Inc. All rights reserved.
-        </p>
+        <p className="mt-6">&copy; 2024 TrackBit, Inc. All rights reserved.</p>
       </div>
     </footer>
   );
